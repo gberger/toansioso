8d36b016bd1eb1b8371384114002c30698dde9a6
Guilherme Berger <guilherme.berger@gmail.com>
2014-05-01 23:39:44 -0300

    Better error checking and handling.

--- toa.js
+++ toa.js
@@ -1,10 +1,16 @@
 var colors = require('colors')
 var Browser = require('zombie');
-var config = require('./config.json');
 
-if(!config || !config.email || !config.password) {
-  console.error("Put your email and password in JSON format in config.json!\nSee the README for more info.".red)
+try {
+  var config = require('./config.json');
+} catch(err) {
+  console.error("Couldn't load config.json. Does is it exist and is in the correct format?\nSee the README for more info.".red)
   process.exit(1);
+}
+
+if(!config || !config.email || !config.password || !config.url) {
+  console.error("Put your email and password in JSON format in config.json!\nSee the README for more info.".red)
+  process.exit(2);
 }
 
 var browser = new Browser();
@@ -20,12 +26,27 @@ browser.visit(config.url).then(function(){
     return browser.pressButton('Log In');  
   } 
 }).then(function(){
+  if(browser.query('#header_errors li')){
+    console.log('Wrong username or password!'.red)
+    process.exit(3);
+  } 
+
   console.log('Logged in!'.grey);
+
   var html = browser.html();
-  var docs = html.match(/Documents you have uploaded (\(\d+\))/)[1];
+  var match = html.match(/Documents you have uploaded (\(\d+\))/);
+  if(match === null || !match[1]) {
+    console.log("Couldn't find the number of uploaded documents! Are you sure the URL is correct?\nSee the README for more info.".red)
+    process.exit(4);
+  }
+
+  docs = match[1];
+
   if(docs === '(1)') {
     console.log(docs.blue);
   } else {
     console.log(docs.green);
   }
+
+  process.exit(0);
 });
