c9c2f5ba05e91242969c7d4881073f6b00f47726
Guilherme Berger <guilherme.berger@gmail.com>
2014-05-02 14:38:33 -0300

    Typo

--- app/register.js
+++ app/register.js
@@ -41,7 +41,7 @@ module.exports = function(req, res) {
             html: '<b>Olá!</b> Agora fique tranquilo que lhe enviaremos um email quando seu TOA chegar. <br>' +
                   'Caso queira CANCELAR seu cadastro no TOAnsioso, clique em: <a href="'+url+'">'+url+'</a>. ' +
                   'Isso não afeta sua inscrição no CSF.'
-          }
+          });
         });
       }, function(reason){
         //error
