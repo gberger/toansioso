92b69c37cbbc620b046f251cfa66845da88ffae4
Guilherme Berger <guilherme.berger@gmail.com>
2014-06-12 01:32:35 -0300

    slowly

--- app/go-check.js
+++ app/go-check.js
@@ -1,21 +1,35 @@
 var checker = require('./checker');
 var mailer = require('./mailer');
 var async = require('async');
+var moment = require('moment');
 
 module.exports = function(db, concurrency) {
   console.log('Starting check...');
 
   var users = db.get('users');
 
+  var section = parseInt(moment().minutes()/10);
+
   users.find({done: false}, function(err, all){
-    var tasks = all.map(function(user){
+
+    var total = all.length;
+    var per = Math.ceil(total/6);
+    var min = section*per;
+    var max = (section+1)*per;
+
+    var tasks = all.map(function(user, i){
+
+      if(i < min || i > max) {
+        console.log('Skipping user ' + user.email + ' (' + i + ') for now.')
+      }
+
       if(user.n === null || user.n === undefined){
         user.n = 1;
       }
       return function(callback){
         console.log('Checking user ' + user.email + ' (' + user.n + ')');
         checker(user.email, user.password, user.url).then(function(n){
-          console.log('Checked  user ' + user.email);
+          console.log('Checked  user ' + user.email + ' (' + n + ')');
           if(n > user.n){
             console.log('Sending mail to user ' + user.email)
             mailer({
